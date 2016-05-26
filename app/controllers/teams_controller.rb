70593867048689d1306311c5c0db9371812e7d17
Operation BB <jaycheezy@gmail.com>
2016-05-26 19:01:38 +0200

    added teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,51 @@
+class TeamsController < ApplicationController
+ 	before_action :find_team, only: [:show, :edit, :update, :destroy]
+	before_action :find_country, only: [:show, :edit, :update, :destroy]
+
+	def create
+		@team = Team.new team_params
+
+		if @team.save
+			redirect_to @team, notice: "Hellz Yea Chappy! New Team Added"
+		else
+			render 'new', notice: "Awwwh Shit"
+		end
+	end
+
+	def show
+		@countries = Country.all.order("title ASC")
+		@leagues = League.all
+		@teams = Team.all.order("title ASC")
+	end
+
+
+	def edit
+	end
+
+	def update
+		if @team.update team_params
+			redirect_to @team, notice: "team was saved"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@team.destroy
+		redirect_to countries_path
+	end
+
+	private
+
+	def team_params
+		params.require(:team).permit(:title)
+	end
+
+	def find_team
+		@team = Team.find(params[:id])
+	end
+	def find_country
+		@country = Country.find(params[:id])
+	end
+
+end
