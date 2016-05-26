50408c046c4af1edf137d796e5f810a9126660a7
Operation BB <jaycheezy@gmail.com>
2016-05-26 16:41:13 +0200

    country controls

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -9,6 +9,38 @@ class CountriesController < ApplicationController
 		@country = Country.new
 	end
 
+	def create
+		@country = Country.new country_params
+
+		if @country.save
+			redirect_to @country, notice: "Hellz Yea Chappy! New Country Added"
+		else
+			render 'new', notice: "Awwwh Shit"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @country.update country_params
+			redirect_to @country, notice: "country was saved"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@country.destroy
+		redirect_to countries_path
+	end
+
+
+	
+
 	private
 
 
