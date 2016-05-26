b23fbb018155d039379ef00300ad3f708ecf3c45
Operation BB <jaycheezy@gmail.com>
2016-05-26 16:25:01 +0200

    new countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,25 @@
 class CountriesController < ApplicationController
-  def home
-  	 @countries = Country.all.order("title ASC")
-  end
+	before_action :find_country, only: [:show, :edit, :update, :destroy]
+
+    def home
+  		@countries = Country.all.order("title ASC")
+    end
+
+	def new
+		@country = Country.new
+	end
+
+	private
+
+
+	def country_params
+		params.require(:country).permit(:title)
+	end
+
+	def find_country
+		@country = Country.find(params[:id])
+	end
+
+
+
 end
