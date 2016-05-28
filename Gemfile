f5ea78fb73559b20344bf90d0a8845e5f4bde2bc
Operation BB <jaycheezy@gmail.com>
2016-05-28 17:51:37 +0200

    changed dev db to pg, and restored from heroku

--- Gemfile
+++ Gemfile
@@ -43,7 +43,7 @@ gem "figaro"
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  gem 'sqlite3'
+  gem 'pg'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160526172341) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "countries", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",        null: false
